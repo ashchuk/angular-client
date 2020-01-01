025c8e59b46a9aaaed4f744c8b87808de393be5e
Artyom Koshko <androidakbluebox@gmail.com>
2020-01-01 17:04:52 +0300

    Add some http functionality

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,7 +1,9 @@
-import {HEROES} from '../mock/mock-heroes';
-import {Hero} from '../models/hero';
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of} from 'rxjs';
+import {catchError, map, tap} from 'rxjs/operators';
+
+import {Hero} from '../models/hero';
 import {MessagesService} from './messages.service';
 
 @Injectable({
@@ -9,17 +11,76 @@ import {MessagesService} from './messages.service';
 })
 export class HeroService {
 
-  constructor(private messagesService: MessagesService) {
+  private heroesUrl = 'api/heroes';
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
+  constructor(
+    private messagesService: MessagesService,
+    private http: HttpClient
+  ) {
+  }
+
+  private log(message: string): void {
+    this.messagesService.add(`HeroService: ${message}`);
   }
 
   getHeroes(): Observable<Hero[]> {
     this.messagesService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
+    // return of(HEROES);
   }
-
 
   getHero(id: number): Observable<Hero> {
     this.messagesService.add(`HeroService: fetched hero id id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+    // return of(HEROES.find(hero => hero.id === id));
+  }
+
+  private handleError<T>(operation: string, result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<any>('addHero'))
+      );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions)
+      .pipe(
+        tap(_ => this.log(`deleted hero id=${id}`)),
+        catchError(this.handleError<any>('deleteHero'))
+      );
   }
 }
